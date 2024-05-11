17a8d6319cec81c8b948b83651d26f1a456831bd
francineannealvarez <23-09780@g.batstate-u.edu.ph>
2024-05-11 09:23:38 +0800

    save users

--- utils/user_manager.py
+++ utils/user_manager.py
@@ -2,27 +2,24 @@ import os
 
 class UserManager:
 	def __init__(self, users):
-		self.users = {}
-		self.load_users()
+		self.users = {} #empty dictionary
+		self.load_users() #invoke load_users function
 
 	def load_users(self):
 		if not os.path.exists("users.txt"):
 			os.makedirs("users.txt") #create file
 			f=open("users.txt", "w")
 			f.close()
-			with open("users.txt") as file:
+			with open("users.txt") as file: #with to automatically close without the need of close()
 				for info in file:
 					username, password = info.strip().split(",")
 					self.users[username] = password
-	
-			
-		
-     
-
 
 	def save_users():
-		pass
-
+		with open("users.txt", "a") as file:
+			for username, password in self.users():
+				file.write(f"{username}, {password}\n")
+			 
 	def validate_username():
 		pass
 
