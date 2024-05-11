0fd9d84ba97c367046c20eeb42f0adcce2a2da63
francineannealvarez <23-09780@g.batstate-u.edu.ph>
2024-05-11 11:09:49 +0800

    .

--- utils/dice_game.py
+++ utils/dice_game.py
@@ -1,19 +1,37 @@
 class DiceGame:
-	def load_scores():
-		pass
+	
+    def load_scores():
+        pass
 
-	def save_scores():
-		pass
+    def save_scores():
+        pass
 
-	def play_game():
-		pass
+    def play_game():
+        pass
 
-	def show_top_scores():
-		pass
+    def show_top_scores():
+        pass
+    
+    def logout():
+        pass
+        
+    def menu():
+        pass
 
-	def logout():
-		pass
-
-	def menu():
-		pass
+    def register(self):
+        print("\nRegister User Page")
+        username = input("Enter username (at least 4 characters) or leave blank to cancel: ")
+        if not username.strip():  # Check if input is blank
+            print("Register failed.")
+            return
+        password = input("Enter password (at least 8 characters) or leave blank to cancel: ")
+        if not password.strip():  # Check if input is blank
+            print("Register failed.")
+            return
+        if self.user_manager.register(username, password):
+            print("Registered successfully.")
+        else:
+            print("Registration failed. Please try again.")
+	
+    
 		
--- utils/user_manager.py
+++ utils/user_manager.py
@@ -1,47 +1,50 @@
 import os
 
 class UserManager:
-	def __init__(self, users):
-		self.users = {} #empty dictionary
-		self.load_users() #invoke load_users function
+    def __init__(self, users):
+        self.users = {} #empty dictionary
+        self.load_users() #invoke load_users function
 
-	def load_users(self):
-		if not os.path.exists("users.txt"):
-			os.makedirs("users.txt") #create file
-			f=open("users.txt", "w")
-			f.close()
-			with open("users.txt") as file: #with to automatically close without the need of close()
-				for info in file:
-					username, password = info.strip().split(",")
-					self.users[username] = password
+    def load_users(self):
+        if not os.path.exists("users.txt"):
+            os.makedirs("users.txt") #create file
+            f=open("users.txt", "w")
+            f.close()
+            with open("users.txt") as file: #with to automatically close without the need of close()
+                for info in file:
+                    username, password = info.strip().split(",")
+                    self.users[username] = password
 
-	def save_users(self):
-		with open("users.txt", "a") as file:
-			for username, password in self.users():
-				file.write(f"{username}, {password}\n")
+    def save_users(self):
+        with open("users.txt", "a") as file:
+            for username, password in self.users():
+                file.write(f"{username}, {password}\n")
 			 
-	def validate_username(self, username):
-		if len(username) >= 4 and username not in self.users:
-			return True
-		else:
-			return False
+    def validate_username(self, username):
+        if len(username) >= 4:
+            if username not in self.users:
+                return True
+            else:
+                print("Username already exists.")
+        else:
+            print("Username must be at least 4 characters long.")
 
-	def validate_password(self, password):
-		if len(password) >= 8:
-			return True
-		else:
-			return False
+    def validate_password(self, password):
+        if len(password) >= 8:
+            return True
+        else:
+            print("Password must be at least 8 characters long.")
 
-	def register(self, username, password):
-		if self.validate_username and self.validate_password:
-			self.users[username] = password
-			self.save_users()
-			return True
-		else:
-			return False
+    def register(self, username, password):
+        if self.validate_username and self.validate_password:
+            self.users[username] = password
+            self.save_users()
+            return True
+        else:
+            return False
 
-	def login(self, username, password):
-		if username in self.users and self.users[username]["password"] == password:
-			return True
-		else:
-			return False
+    def login(self, username, password):
+        if username in self.users and self.users[username]["password"] == password:
+            return True
+        else:
+            return False
