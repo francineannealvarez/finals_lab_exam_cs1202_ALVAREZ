b2388af566a475e8e44664f3db611898d4c196ea
francineannealvarez <23-09780@g.batstate-u.edu.ph>
2024-05-11 09:17:41 +0800

    load users

--- utils/user_manager.py
+++ utils/user_manager.py
@@ -1,10 +1,24 @@
+import os
+
 class UserManager:
 	def __init__(self, users):
 		self.users = {}
 		self.load_users()
 
-	def load_users():
-		pass
+	def load_users(self):
+		if not os.path.exists("users.txt"):
+			os.makedirs("users.txt") #create file
+			f=open("users.txt", "w")
+			f.close()
+			with open("users.txt") as file:
+				for info in file:
+					username, password = info.strip().split(",")
+					self.users[username] = password
+	
+			
+		
+     
+
 
 	def save_users():
 		pass
